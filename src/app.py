c5baf9998e628918e9bda53927c8c1d78117e484
ballakeita <ballakei070@gmail.com>
2026-09-15 21:47:47 +0200

    add modif

---
+++ src/app.py
@@ -0,0 +1,26 @@
+# '/api/v1/details'
+# '/api/v1/health'
+
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        
+        'time' : datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+        
+        'hostname' : socket.gethostname()
+    })
+    
+@app.route('/api/v1/health')
+def health():
+    return jsonify({
+        
+        'status' : 'up'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
